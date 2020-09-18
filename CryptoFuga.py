20b052882607e0a835d0c70a04bb63fdea89b1e0
th30c05 <arnaufugarolas@gmail.com>
2020-09-18 21:10:35 +0200

    Key class finish.

--- CryptoFuga.py
+++ CryptoFuga.py
@@ -2,6 +2,21 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
 
-def Key_Generator(Bytes):
-    key = get_random_bytes(Bytes)
-    return key
+class Key:
+    def __init__(self):
+        self.key = None
+
+    def Key_Generator(self, Bytes):
+        self.key = get_random_bytes(Bytes)
+        return self.key
+
+    def Key_Loader(self, Name):
+        key_file = open(Name, "rb")
+        self.key = key_file.read()
+        key_file.close()
+        return self.key
+
+    def Key_Save(self):
+        key_file = open("key", "wb")
+        key_file.write(self.key)
+        key_file.close()
