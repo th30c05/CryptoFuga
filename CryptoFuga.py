75dd4419992018a87e41398e7beca2e5b5326dd3
th30c05 <arnaufugarolas@gmail.com>
2020-09-24 20:39:43 +0200

    Convert Key_Generator to Bytes_Generator (for AES key and IV).

--- CryptoFuga.py
+++ CryptoFuga.py
@@ -2,9 +2,10 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
 
-def Key_Generator(Bytes):
-    key = get_random_bytes(Bytes)
-    return key
+def Bytes_Gen(Bytes):
+    Bytes = get_random_bytes(Bytes)
+    return Bytes
+
 
 
 def Key_Loader(File_Name):
